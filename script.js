2758dfd51492902d067189903818d4e8bc9f4304
JSShant <joshua.shanthikumar@outlook.com>
2022-07-15 18:48:36 +1000

    Implement Lazy load of img for website performance

--- script.js
+++ script.js
@@ -119,3 +119,26 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+//Lazy image loading for website performance
+const imgTarget = document.querySelectorAll("img[data-src]");
+console.log(imgTarget);
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  //replacing src with data-src
+  entry.target.src = entry.target.dataset.src;
+  //load event
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+imgTarget.forEach(function (img) {
+  imgObserver.observe(img);
+});
