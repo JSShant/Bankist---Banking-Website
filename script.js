241ef828ccc62c2f0542116b7bc17caff729d414
JSShant <joshua.shanthikumar@outlook.com>
2022-07-21 01:50:01 +1000

    Fixed issue where smooth scrolling was not working

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ scrollBtn.addEventListener("click", function (e) {
 
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
-  if (e.target.classList.contains("nav__link" && !"btn__show--modal")) {
+  if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
